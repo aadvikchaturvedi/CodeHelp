ffa2ca3b9a401913efa98f2470d89cdccb95307a
aadvikchaturvedi <aadvikchaturvedi06@gmail.com>
2026-04-18 18:08:26 +0530

    chatbot frontend intialise and fixing some bugs

---
+++ src/components/Chatbox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Chatbox() {
+  return (
+    <div className='w-full h-full bg-zinc-950 border-l border-zinc-800/50 flex flex-col overflow-hidden backdrop-blur-sm'>
+        
+    </div>  
+  )
+}
